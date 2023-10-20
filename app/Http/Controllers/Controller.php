f4f7faf53f868a59ec450dae8be24ed1d0bb31cc
unknown <UFG@CC1-10.ufg.local>
2023-10-19 19:54:22 -0600

    Documentacion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+*@OA\Info(
+*    version="1.0.0",
+*        title="API de Daniel Ernesto Lopez Sibrian",
+*        description="API de ejemplo para la clase de desarrollo de backend y apis",
+*        @OA\Contact(
+*           email="ia.daniellopez1@ufg.edu.sv",
+ *           name="Daniel Ernesto Lopez Sibrian",
+ *           url="https://localhost/api/"
+  *      ),
+ *       @OA\License(
+  *          name="Apache 2.0",
+   *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+   *     )
+*)
+*/
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/respuestaController.php
+++ app/Http/Controllers/respuestaController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+
+
 class respuestaController extends Controller
 {
+
+/**
+*
+*Descripcion de prueba para la clase de desarrollo de backend y api desarrollada de la clase 19/10
+*@return \Illuminate\Http\Response
+*
+*@OA\Get(
+*    path="/api/ejecutar",
+*    tags={"ejecutar"},
+*    summary="Esta es un endpoint/metodo para obtener datos y mostrarlos al usuario",
+*    @OA\Response(
+*        response=200,
+*        description="Se devuelven todos los registros cuando la consulta sea satisfactoria"
+*    ),
+*      @OA\Response(
+*          response=500,
+*          description="No se pudo realizar la ejecución al parecer hubo un error interno"
+*      )
+*  ) 
+*/
+
     public function index(){
         return response() ->json([
             "success"=>true,
