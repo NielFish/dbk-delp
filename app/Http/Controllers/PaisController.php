ed12a6dda1a6942f43a8ba3bb66d2ee917cd1dc0
Niel <danielopez225@gmail.com>
2023-11-09 15:44:16 -0600

    Modificar Pais

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -7,6 +7,27 @@ use App\Models\Pais;
 
 class PaisController extends Controller
 {
+
+/**
+*
+*Descripcion de prueba para la clase de desarrollo de backend y api desarrollada de la clase 19/10
+*@return \Illuminate\Http\Response
+*
+*@OA\Get(
+*    path="/api/obtener",
+*    tags={"obtener"},
+*    summary="Esta es un endpoint/metodo para obtener datos y mostrarlos al usuario",
+*    @OA\Response(
+*        response=200,
+*        description="Se devuelven todos los registros cuando la consulta sea satisfactoria"
+*    ),
+*      @OA\Response(
+*          response=500,
+*          description="No se pudo realizar la ejecución al parecer hubo un error interno"
+*      )
+*  ) 
+*/
+
     public function obtenerPais(){
         $Pais = new Pais();
         $data = $Pais::All();
@@ -18,6 +39,26 @@ class PaisController extends Controller
             "cantidad"=>1
         ]);
     }
+
+    /**
+*
+*Descripcion de prueba para la clase de desarrollo de backend y api desarrollada de la clase 19/10
+*@return \Illuminate\Http\Response
+*
+*@OA\Post(
+*    path="/api/nuevo",
+*    tags={"nuevo"},
+*    summary="Esta es un endpoint/metodo para obtener datos y mostrarlos al usuario",
+*    @OA\Response(
+*        response=200,
+*        description="Se devuelven todos los registros cuando la consulta sea satisfactoria"
+*    ),
+*      @OA\Response(
+*          response=500,
+*          description="No se pudo realizar la ejecución al parecer hubo un error interno"
+*      )
+*  ) 
+*/
 
     public function crearPais(Request $request){
         $Pais = new Pais();
@@ -51,5 +92,29 @@ class PaisController extends Controller
             ]);
         }
     }
+
+    public function modificarPais(Request $request, $id){
+        $Pais = Pais::find($id);
+    
+        if ($Pais) {
+            $Pais->nombre_pais = $request->nombre;
+            $Pais->save();
+    
+            return response()->json([
+                "success" => true,
+                "message" => "País modificado exitosamente",
+                "data" => $Pais,
+                "cantidad" => 1
+            ]);
+        } else {
+            return response()->json([
+                "success" => false,
+                "message" => "El país no se encontró o no se pudo modificar.",
+                "data" => null,
+                "cantidad" => 0
+            ]);
+        }
+    }
+    
 
 }
--- routes/api.php
+++ routes/api.php
@@ -30,11 +30,35 @@ use App\Http\Controllers\PaisController;
 
 Route::get("/ejecutar", [respuestaController::class, "index"]);
 
+/**
+ * 
+ * 
+ * @OA\Post(
+  *      path="/api/nuevo",
+   *     summary="Endpoint para insertar datos",
+    *    @OA\Response(response="200", description="Datos devueltos"),
+    *)
+ * 
+ */
+
 Route::post("/nuevo",[PaisController::class,"crearPais"]);
+
+/**
+ * 
+ * 
+ * @OA\Get(
+  *      path="/api/obtener",
+   *     summary="Endpoint para la consulta de datos",
+    *    @OA\Response(response="200", description="Datos devueltos"),
+    *)
+ * 
+ */
 
 Route::get("/obtener",[PaisController::class,"obtenerPais"]);
 
 Route::delete("/eliminar/{id}", [PaisController::class, "eliminarPais"]);
+
+Route::put("/modificar/{id}", [PaisController::class, "modificarPais"]);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
